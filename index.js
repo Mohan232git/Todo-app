72f02568e721fc9ed39fe2fa74e65ec47a5e1f07
Gituser <pvmohan232@gmail.com>
2023-06-04 09:15:34 +0530

    js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-
+document.getElementById("input-box").addEventListener("keydown" , function(event){
+    if(event.keyCode==13) {
+        appendli();
+    }
+})
 
 function appendli() {
     let input = document.getElementById("input-box") ;
@@ -7,34 +11,57 @@ function appendli() {
     let label = document.createElement("label");
     let li = document.createElement("li");
     let liList =document.getElementById("ul-container");
+    let span2 = document.createElement("span") ;
+
+    if(input.value==""){
+        input.classList.add("invalid")
+    }
+    else{
+    
     span.classList.add("check");
     div.append(span);
     label.innerHTML = input.value ;
+    label.id = "list-items" ;
     div.appendChild(label);
     div.classList.add("task");
     li.appendChild(div);
-    span.classList.add("cross");
-  /*  li.appendChild(span);
-    liList.appendChild(li); */
-    console.log(div)
+    span2.classList.add("cross");
+    li.appendChild(span2);
+    li.classList.add("listItems");
+    liList.appendChild(li);
     input.value="" ;
+    input.classList.remove("invalid");
+    
+    }
+    
 }
 function del() {
 
 }
 
 
-let input =document.getElementById("input-box").addEventListener("keydown" , function(event){
-    if(event.keyCode==13) {
-        appendli();
-    }
+
+let labels = document.getElementById("list-items")
+console.log(labels)
+labels.addEventListener(onclick , function(event){
+        let span = document.getElementsByClassName("check");
+        console.log(span)
 })
 
-function div(){
-   
-let span = document.createElement("span");
-let label = document.createElement("label") ;
-div.appendChild(span);
-div.appendChild(label);
- return div ;
-}
+let spanCross = document.getElementsByClassName("cross");
+let liList =document.getElementById("ul-container");
+let listItem = document.getElementsByClassName("listItems")
+console.log(listItem);
+liList.addEventListener(onclick,function(event){
+     if(event.target.tagName==" ") {
+        console.log(event.target);
+
+     }
+})
+
+
+   /* document.getElementsByClassName("task").addEventListener(onclick,
+    function(e){
+        console.log('cliked')
+    })*/
+
